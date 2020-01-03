aa98476a510f1c9d6fd83d72bf2744a6d4e36ab5
dionysius14 <damaragatedionysius@gmail.com>
2020-01-03 09:44:08 +0700

    remove

--- application/views/slices/footer_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<div style="height:50px"></div>
-<footer class="footer">
-    <div class="container">
-        <p>Copyright &copy; <a href="#"></a> 2019.</p>
-    </div>
-</footer>
